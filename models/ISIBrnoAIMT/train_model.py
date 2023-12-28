396e5adab2ec551796fa9274a92f94f7ea058628
Rino <rino_gantenbein@hotmail.com>
2023-12-28 11:43:06 +0100

    remove comments

--- models/ISIBrnoAIMT/train_model.py
+++ models/ISIBrnoAIMT/train_model.py
@@ -6,11 +6,7 @@ from team_code import training_code
 
 if __name__ == '__main__':
     # Parse arguments.
-    # if len(sys.argv) != 3:
-    #     raise Exception('Include the data and model folders as arguments, e.g., python train_model.py data model.')
-    #
-    # data_directory = sys.argv[1]
-    # model_directory = sys.argv[2]
+
     os.chdir('../../')
     data_directory = './cinc-2021_data/'
     model_directory = './models/ISIBrnoAIMT/'
